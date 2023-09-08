6096b6a952f2bacd9c3abc0d05457cb23fba254a
Jacob Hoffmann <67609232+jacobhoffmann257@users.noreply.github.com>
2023-09-08 01:23:47 +0000

    game done

--- app.rb
+++ app.rb
@@ -6,13 +6,43 @@ get("/") do
 end
 get("/rock") do
   rps = ["Rock","Paper","Scissors"]
+  @comp_move = rps.sample
+  @ai_play = "They played #{@comp_move}!"
+  @outcome
+  if@comp_move == "Rock"
+    @outcome = "Tied"
+  elsif@comp_move == "Scissors"
+    @outcome = "Won"
+  else
+    @outcome = "Lost"
+  end
   erb(:rock)
 end
 get("/scissors") do
   rps = ["Rock","Paper","Scissors"]
-erb(:scissors)
+  @comp_move = rps.sample
+  @ai_play = "They played #{@comp_move}!"
+  @outcome
+  if@comp_move == "Rock"
+    @outcome = "Lost"
+  elsif@comp_move == "Scissors"
+    @outcome = "Tied"
+  else
+    @outcome = "Won"
+  end
+  erb(:scissors)
 end
 get("/paper") do
   rps = ["Rock","Paper","Scissors"]
+  @comp_move = rps.sample
+  @ai_play = "They played #{@comp_move}!"
+  @outcome
+  if@comp_move == "Rock"
+    @outcome = "Won!"
+  elsif@comp_move == "Scissors"
+    @outcome = "Lost"
+  else
+    @outcome = "Tied"
+  end
   erb(:paper)
 end
